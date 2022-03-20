be80bf8ef1cdc5b165423198cf62f2186f7a701a
Иван <88025729+polis4712@users.noreply.github.com>
2022-03-20 17:25:37 +0300

    Add files via upload

---
+++ js/tabs.js
@@ -0,0 +1,36 @@
+const tabButtons = document.querySelectorAll(".design-list__item");
+const tabDescriptions = document.querySelectorAll(".design__descr");
+const tabImages = document.querySelectorAll(".design-images");
+const tabDesign = document.querySelectorAll(".design-block__img");
+const tabTitle = document.querySelectorAll(".design__title");
+
+const changeContent = (array, value) => {
+  array.forEach((elem) => {
+    if (elem.dataset.tabsField === value) {
+      elem.classList.remove("hidden");
+    } else {
+      elem.classList.add("hidden");
+    }
+  });
+};
+
+tabButtons.forEach((tabButton) => {
+  tabButton.addEventListener("click", (event) => {
+    const dataValue = tabButton.dataset.tabsHandler;
+
+    changeContent(tabDescriptions, dataValue);
+    changeContent(tabImages, dataValue);
+    changeContent(tabDesign, dataValue);
+    changeContent(tabTitle, dataValue);
+
+    tabButtons.forEach((btn) => {
+      console.log(btn);
+      console.log(event.target);
+      if (btn === event.target) {
+        btn.classList.add("design-list__item_active");
+      } else {
+        btn.classList.remove("design-list__item_active");
+      }
+    });
+  });
+});
